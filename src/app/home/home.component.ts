511c26869d3023d8daa03ced25d7f9b327d8e870
Nancy Ahmed <nancyahmed2025@gmail.com>
2022-03-17 22:16:53 +0200

    homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
+import { MoviesService } from './../movies.service';
 import { Component, OnInit } from '@angular/core';
-
+MoviesService
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  trendingMovies:any[] =[];
+  trendingTv:any[]=[];
+  trendingpeople:any[]=[];
+
+
+  imgPrefix:string ='https://image.tmdb.org/t/p/w500';
+  constructor(private _MoviesService:MoviesService) { }
 
   ngOnInit(): void {
+      this._MoviesService.getTrending('movie').subscribe((response) =>{
+        this.trendingMovies = response.results.slice(0,10);
+      })
+
+      this._MoviesService.getTrending('tv').subscribe((response) =>{
+        this.trendingTv = response.results.slice(0,10);
+      })
+
+      this._MoviesService.getTrending('person').subscribe((response) =>{
+        this.trendingpeople = response.results.slice(0,10);
+      })
+
+
   }
 
 }
---
+++ src/app/movies.service.ts
@@ -0,0 +1,16 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+
+  constructor(private _HttpClient:HttpClient) { }
+  getTrending(mediaType:string):Observable<any>{
+      return this._HttpClient.get(`https://api.themoviedb.org/3/trending/${mediaType}/week?api_key=5f9bfdd384783aea5a70458264e1c916`)
+  }
+}
